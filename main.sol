6a91c419504a7e919e07c1c4f2da26ad7f567eae
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-02-23 15:58:57 +0530

    Added test and Updated getter and setter in main

--- main.sol
+++ main.sol
@@ -7,7 +7,7 @@ contract Anticorruption{
         // Basic info of every entity
         string Name;
         uint BankAccountNo;
-        uint Validity;
+        bool Validity;
         
     }
     
@@ -21,41 +21,55 @@ contract Anticorruption{
         mapping(uint => uint) MoneyAllocatedForEntityForPerticularEntity;
         mapping(uint => uint) MoneyReceived;
         uint [] Authorized; 
-        uint Validity;
+        bool Validity;
 
     }
     
     mapping(uint => BasicInfoOfEntity) EntityInfo;
     mapping(uint => SchemesInfo) Schemes; 
     mapping(uint => uint) TotalMoney;
+    mapping(uint => bool) BankAccount;
     
-    function AddEntity(uint AdharcardNo, string _name, uint _BankAccountNo)  returns (string Status) { 
+    function AddEntity(uint AdharcardNo, string _name, uint _BankAccountNo)  returns (string Status ) { 
         // Add new Entity
-        if (EntityInfo[AdharcardNo].Validity==0){ 
+        if (EntityInfo[AdharcardNo].Validity==false && BankAccount[_BankAccountNo] == false){ 
         EntityInfo[AdharcardNo].Name = _name;
         EntityInfo[AdharcardNo].BankAccountNo = _BankAccountNo;
-        EntityInfo[AdharcardNo].Validity = 1;
-        return("  Entity Added Successfully.");
+        EntityInfo[AdharcardNo].Validity = true;
+        return(" Entity Added Successfully.");
        }
        else{
-        return("  Entity Already present.");
+        return(" Entity Already present.");
        }
     }
     
     function GetEntity(uint AdharcardNo) view returns (string Name ,uint BankAccountNo ) {
         // Get info About Entity
-        require(EntityInfo[AdharcardNo].Validity==1);
+        require(EntityInfo[AdharcardNo].Validity==true);
         return(EntityInfo[AdharcardNo].Name,EntityInfo[AdharcardNo].BankAccountNo);
     }
     
-    function AddScheme(string _name,uint _amount)  returns (string Status)  {
+    function AddScheme(string SID,string _name,string _Description,uint _amount,uint _BankAccountNo)  returns (string Status)  {
         // Add new Schemes
-        
-        
+        uint sid = uint(keccak256(SID));
+        if (Schemes[sid].Validity==false && BankAccount[_BankAccountNo] == false ){
+            Schemes[sid].Name = _name;
+            Schemes[sid].Description = _Description;
+            Schemes[sid].TotalAmount = _amount;
+            Schemes[sid].BankAccountNo = _BankAccountNo;
+            Schemes[sid].Validity = true;
+            return(" Scheme Created Successfully.");
+        }
+        else{
+            return(" Scheme ID Already Present.");
+        }
     }
     
-    function GetScheme(string name) view returns (string Description ,uint TotalAmount , uint BankAccountNo, uint []) {
+    function GetScheme(string SID) view returns (string Description ,uint TotalAmount , uint BankAccountNo, uint [] Authorized) {
         // Get Schemes
+        uint sid = uint(keccak256(SID));
+        require(Schemes[sid].Validity==true);
+        return(Schemes[sid].Description,Schemes[sid].TotalAmount,Schemes[sid].BankAccountNo,Schemes[sid].Authorized);
     }
     
     function AddAuthorizedPerson(uint AdharcardNo)  returns (string Status) {
