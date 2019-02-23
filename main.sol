a035393ac972fd007d5bd6b47e3f9c1f6a7ba73c
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-02-23 14:18:04 +0530

    Updated main.sol

--- main.sol
+++ main.sol
@@ -7,6 +7,7 @@ contract Anticorruption{
         // Basic info of every entity
         string Name;
         uint BankAccountNo;
+        uint Validity;
         
     }
     
@@ -19,55 +20,70 @@ contract Anticorruption{
         mapping(uint => bool) AuthorizedEntity;
         mapping(uint => uint) MoneyAllocatedForEntityForPerticularEntity;
         mapping(uint => uint) MoneyReceived;
+        uint [] Authorized; 
+        uint Validity;
+
     }
     
     mapping(uint => BasicInfoOfEntity) EntityInfo;
     mapping(uint => SchemesInfo) Schemes; 
     mapping(uint => uint) TotalMoney;
     
-    function AddEntity(uint AdharcardNo, string name, address BankAccountNo) { 
+    function AddEntity(uint AdharcardNo, string _name, uint _BankAccountNo)  returns (string Status) { 
         // Add new Entity
+        if (EntityInfo[AdharcardNo].Validity==0){ 
+        EntityInfo[AdharcardNo].Name = _name;
+        EntityInfo[AdharcardNo].BankAccountNo = _BankAccountNo;
+        EntityInfo[AdharcardNo].Validity = 1;
+        return("  Entity Added Successfully.");
+       }
+       else{
+        return("  Entity Already present.");
+       }
     }
     
-    function GetEntity(uint AdharcardNo) returns (string Name ,uint BankAccountNo ) {
+    function GetEntity(uint AdharcardNo) view returns (string Name ,uint BankAccountNo ) {
         // Get info About Entity
+        require(EntityInfo[AdharcardNo].Validity==1);
+        return(EntityInfo[AdharcardNo].Name,EntityInfo[AdharcardNo].BankAccountNo);
     }
     
-    function AddScheme(string _name,uint _amount) returns (string Status) {
+    function AddScheme(string _name,uint _amount)  returns (string Status)  {
         // Add new Schemes
+        
         
     }
     
-    function GetScheme(string name) {
+    function GetScheme(string name) view returns (string Description ,uint TotalAmount , uint BankAccountNo, uint []) {
         // Get Schemes
     }
     
-    function AddAuthorizedPerson(uint AdharcardNo) returns (string Status) {
+    function AddAuthorizedPerson(uint AdharcardNo)  returns (string Status) {
         // AddAuthorizedPerson
     }
     
-    function AddAuthorizedCompany(uint CIN) returns (string Status) {
+    function AddAuthorizedCompany(uint CIN)  returns (string Status) {
         // AddAuthorizedCompany
     }
     
-    function MoneyForEntityForPerticularSchemes(string name) returns (uint Money) {
+    function MoneyForEntityForPerticularSchemes(string name) view returns (uint Money) {
         // GetMoneyForEntityForPerticularSchemes
     }
     
-    function TotalMoneyForEntity(string name) returns (uint Money) {
+    function TotalMoneyForEntity(string name) view returns (uint Money) {
         // TotalMoneyForEntity
     }
     
     
-    function MoneyToNextLevel() returns (string Status) {
+    function MoneyToNextLevel()  returns  (string Status) {
         // MoneyToNextLevel
     }
     
-    function MoneyAPersonGetting() returns (uint Money) {
+    function MoneyAPersonGetting() view returns (uint Money) {
         // MoneyAPersonGetting
     }
     
-    function TransferMoney(uint From, uint To) returns (string Status) {
+    function TransferMoney(uint From, uint To)  returns (string Status) {
         // TransferMoney
     } 
 }
